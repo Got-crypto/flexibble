33f7d6ba0607f956de699a819b20a16f990889a7
Got-crypto <pecpalmplay@gmail.com>
2023-06-30 15:45:32 +0200

    added layout for the application; included Navbar && Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Flexibble',
@@ -14,7 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Navbar />  
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   )
 }
