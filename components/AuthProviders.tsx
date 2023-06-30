a30c893930994adea8dac144d11ac0fed4c41ae3
Got-crypto <pecpalmplay@gmail.com>
2023-06-30 18:46:55 +0200

    setting up grafbase and NextAuth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,8 +1,43 @@
+"use client"
+
+import { getProviders } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
+  const [providers, setProviders] = useState<Providers | null>()
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      console.log('res', res)
+
+      setProviders(res)
+    }
+
+    fetchProviders()
+  }, [])
+
+  if(providers) {
+    return (
+      <div>
+        {Object.values(providers).map((providers: Provider, i) => (
+          <button key={i}>{providers.id}</button>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
