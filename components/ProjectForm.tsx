3e278e367e08655fc5aafa3e53423d153412dd08
Got-crypto <pecpalmplay@gmail.com>
2023-07-01 19:52:01 +0200

    added a modal UI; creating a Form Project

---
+++ components/ProjectForm.tsx
@@ -0,0 +1,15 @@
+import { SessionInterface } from "@/common.types"
+
+type Props = {
+    type: string,
+    session: SessionInterface
+}
+
+
+function ProjectForm({type, session}: Props) {
+  return (
+    <div>ProjectForm</div>
+  )
+}
+
+export default ProjectForm
