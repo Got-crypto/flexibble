0509ed21a30b7c715d25f6d96aa8d0d25118a148
Got-crypto <pecpalmplay@gmail.com>
2023-07-03 23:52:03 +0200

    adding custom form fields functional components

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,4 +1,9 @@
+"use client"
+
 import { SessionInterface } from "@/common.types"
+import Image from "next/image"
+import { ChangeEvent } from "react"
+import FormField from "./FormField"
 
 type Props = {
     type: string,
@@ -7,8 +12,51 @@ type Props = {
 
 
 function ProjectForm({type, session}: Props) {
-  return (
-    <div>ProjectForm</div>
+    const handleSubmit = (e: React.FormEvent) => {}
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {}
+
+    const handleStateChange = (fieldName: string, value: string) => {
+
+    }
+  
+    const form = {
+        image: '',
+        title: ''
+    }
+    return (
+    <form
+        onSubmit={handleSubmit}
+        className="flextart form"
+    >
+        <div className="flexStart form_image-container">
+            <label htmlFor="poster" className="flexCenter form_image-label">
+                {!form.image && 'Choose a poster for your project'}
+            </label>
+            <input
+                id="image"
+                type="file"
+                accept="image/*"
+               required={type === 'create'}
+               className="form_image-input"
+               onChange={handleChangeImage}
+            />
+            {form.image && (
+                <Image
+                    src={form?.image}
+                    className="sm:p-10 object-contain z-20"
+                    alt='Project Poster'
+                    fill
+                />
+            ) }
+        </div>
+
+        <FormField
+            title='Title'
+            state={form.title}
+            placeholder="Flexibble"
+            setState={(value) => handleStateChange('title', value)}
+        />
+    </form>
   )
 }
 
