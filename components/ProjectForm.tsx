99192ef08458f81f3a783dfe4943611cf4191db7
Got-crypto <pecpalmplay@gmail.com>
2023-07-04 16:01:28 +0200

    finished the project form page

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { SessionInterface } from "@/common.types"
+import { categoryFilters } from "@/constants"
 import Image from "next/image"
-import { ChangeEvent } from "react"
+import { ChangeEvent, useState } from "react"
+import CustomMenu from "./CustomMenu"
 import FormField from "./FormField"
 
 type Props = {
@@ -13,20 +15,42 @@ type Props = {
 
 function ProjectForm({type, session}: Props) {
     const handleSubmit = (e: React.FormEvent) => {}
-    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {}
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+
+        const file = e.target.files?.[0]
+
+        if (!file) return
+        if(!file.type.includes('image')) return alert('Please upload an image file')
+            const reader = new FileReader()
+
+            reader.readAsDataURL(file)
+
+            reader.onload = () => {
+                const result = reader.result as string
+                handleStateChange('image', result)
+            }
+        }
+    
 
     const handleStateChange = (fieldName: string, value: string) => {
+        setForm((prevState) => ({...prevState, [fieldName]: value}))
+    }
 
-    }
-  
-    const form = {
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [form, setForm] = useState({
+        title: '',
+        description: "",
         image: '',
-        title: ''
-    }
+        liveSiteUrl: '',
+        githubUrl: '',
+        category: ''
+    })
+
     return (
     <form
         onSubmit={handleSubmit}
-        className="flextart form"
+        className="flexStart form"
     >
         <div className="flexStart form_image-container">
             <label htmlFor="poster" className="flexCenter form_image-label">
@@ -56,6 +80,37 @@ function ProjectForm({type, session}: Props) {
             placeholder="Flexibble"
             setState={(value) => handleStateChange('title', value)}
         />
+        <FormField
+            title='Description'
+            state={form.description}
+            placeholder="Showcase and discover remarkable developer projects"
+            setState={(value) => handleStateChange('description', value)}
+        />
+        <FormField
+            title='Website URL'
+            type="url"
+            state={form.liveSiteUrl}
+            placeholder="https://example.foo"
+            setState={(value) => handleStateChange('liveSiteUrl', value)}
+        />
+        <FormField
+            type='url'
+            title="Github URL"
+            state={form.githubUrl}
+            placeholder="https://github.com/todo"
+            setState={(value) => handleStateChange('githubUrl', value)}
+        />
+
+        <CustomMenu
+            title="Category"
+            state={form.category}
+            filters={categoryFilters}
+            setState={value => handleStateChange('Category', value)}
+        />
+
+        <div className="flexStart w-full">
+            <button>Create</button>
+        </div>
     </form>
   )
 }
